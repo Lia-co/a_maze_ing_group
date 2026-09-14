ddcab9d0b30ebc1fb3c32865ec63352fdab41c9e
Shiyuan Liang <shiyuan.liang1010@gmail.com>
2026-09-14 16:00:44 +0200

    add comment for bounce check

--- config_parse.py
+++ config_parse.py
@@ -72,7 +72,6 @@ class Config:
 	algorithm: str
 
 
-
 """
 Parse config.txt file to validate KEY=VALUE pair per line and return an immutable class 
 including valid data for later generating a maze.
@@ -136,6 +135,7 @@ def parse_config() -> Config:
 
 	if entry == exit:
 		raise ValueError(f"ENTRY and EXIT are the same. Please make them locating differently.")
+	#bounce check needed more discussion
 	if entry[0] > width or exit[0] > width:
 		raise ValueError(f"ENTRY's x or/and EXIT's x are outside of width. Expect x <= {width}")
 	if entry[1] > height or exit[1] > height:
